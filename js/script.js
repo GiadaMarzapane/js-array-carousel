fe1997bfdd2225a9b9109ebde7058c513e129927
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-23 19:18:47 +0100

    Push Milestone 3

--- js/script.js
+++ js/script.js
@@ -8,16 +8,51 @@ console.log(myImgs);
 
 const myContainerCarousel = document.querySelector('.carousel');
 
+
 for (let index = 0; index < myImgs.length; index++) {
     console.log(index);
 
     const imgCurrent = myContainerCarousel.innerHTML += `<div class="slide">
                                                             <img src="${myImgs[index]}"=>
-                                                        </div>`;
-    console.log(imgCurrent);
-    
+                                                        </div>`;    
     const allSlides = document.querySelectorAll('.slide');
 
     allSlides[0].classList.add("block");
+
+    let imgBlock = 0;
     
+    const next = document.getElementById('next');
+    const previous = document.getElementById('previous');
+
+    next.addEventListener('click',
+        function () {
+                    
+            console.log('click su next');
+            allSlides[imgBlock].classList.remove("block");            
+            imgBlock = imgBlock + 1;
+            allSlides[imgBlock].classList.add("block");
+            previous.classList.remove("hidden");
+
+            if(imgBlock == allSlides.length - 1){
+                next.className = "hidden";
+            }
+        }
+    )
+
+
+    previous.addEventListener('click',
+        function () {
+
+            console.log('click su previous');
+            allSlides[imgBlock].classList.remove("block");
+            imgBlock = imgBlock - 1;
+            allSlides[imgBlock].classList.add("block");
+            
+            if(imgBlock == 0){
+                previous.classList.add("hidden");            
+            }
+
+            next.classList.remove("hidden");
+    }
+    )
 }
